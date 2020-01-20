7c0622da6d7d8c93a613f0859ca1dddab2c16d11
karmina-jc <karmina.jarac@gmail.com>
2020-01-20 12:50:41 -0300

    pagina de login y principio de regsitro en html y DOM

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const root = document.getElementById("root");
+const btnLogIn = document.getElementById("login")
+const btnRegister = document.getElementById("register")
+
+btnRegister.addEventListener('click', () => {
+    root.append(``)
+})
+
