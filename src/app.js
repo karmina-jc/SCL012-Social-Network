dc98248ed393d4866f4ca8dd17cd9e3bcfbda594
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-01-24 12:19:11 -0300

    agregando funcion posteo y google Signin

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ function registrar (){
     });email-password.html
 }
 
+// función de ingreso de sesión 
+
 function ingreso () {
   let email2 = document.getElementById('email2').value;
   let password2= document.getElementById('password2').value;
@@ -57,6 +59,19 @@ function activeUser(){
     <button onclick="cerrar()">Cerrar Sesión</button>
     `;
 }
+//iniciando sesión con Google
+googleSignIn=()=>{
+  base_provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth().signInWithPopup(base_provider).then(function(result){
+    console.log(result)
+    console.log("Success Google Acount Linked");
+  }).catch(function(err){
+    console.log(err)
+    console.log("Failed to do")
+  })
+}
+
+//funcionalidad de Cerrar Sesión
 function cerrar() {
     firebase.auth().signOut()
     .then(function(){
@@ -68,19 +83,61 @@ function cerrar() {
 
 }
 
-//para escribir post
-function posting(){
-  let db = firebase.firestore();
-  db.collection("posteos").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
+
+let db = firebase.firestore();
+
+//agregar usuario en colection
+function guardar(){
+  let nombre= document.getElementById('nombre').value;
+  let seudonimo= document.getElementById('seudonimo').value;
+  let contrasena= document.getElementById('contrasena').value;
+
+  db.collection("newUser").add({
+    firstField: nombre,
+    secondField: seudonimo,
+    tercerField: contrasena
   })
   .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
+    document.getElementById('nombre').value='';
+    document.getElementById('seudonimo').value='';
+    document.getElementById('contrasena').value='';
+
   })
   .catch(function(error) {
     console.error("Error adding document: ", error);
   });
 };
 
+
+//funcionalidad de posteo
+function post() {
+  let post = document.getElementById('posteo').value;
+  
+  db.collection("newPost").add({
+    correo: firebase.auth().currentUser.email,
+    uid: firebase.auth().currentUser.uid,
+    post: post
+    
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    document.getElementById('posteo').value='';
+    
+
+   })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+};
+
+let tabla = document.getElementById('tabla');
+
+db.collection("newUSer").get().then((querySnapshot) => {
+  tabla.innerHTM = '';
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().firstField}`);
+    tabla.innerHTML += `<p> ${doc.id}</p>
+    <p>${doc.data().firstField}</p>`
+  });
+});
