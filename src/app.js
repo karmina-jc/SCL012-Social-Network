64ac0afb5ea80123158157b1dfef5bbfcb6959d7
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-01-28 09:27:47 -0300

    correcion eslinter e inicio de funcion googleSignIn

--- src/app.js
+++ src/app.js
@@ -1,143 +1,99 @@
-function registrar (){
-    let email = document.getElementById('email').value;
-    let password= document.getElementById('password').value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      let errorCode = error.code;
-      let errorMessage = error.message;
-     console.log(errorCode);
-     console.log(errorMessage);
-     
-      // ...
-    });email-password.html
-}
-
-// función de ingreso de sesión 
-
-function ingreso () {
-  let email2 = document.getElementById('email2').value;
-  let password2= document.getElementById('password2').value;
+export function registrar() {
+  const email = document.getElementById('emailRegister').value;
+  const password = document.getElementById('passwordRegister').value;
 
 
-  firebase.auth().signInWithEmailAndPassword(email2, password2).catch(function(error) {
-    // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
+  // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
     console.log(errorCode);
-     console.log(errorMessage);
-    // ...
+    console.log(errorMessage);
+  });
+  email - password.html;
+}
+function activeUser() {
+  const contenido = document.getElementById('contenido');
+  contenido.innerHTML = `
+    <p> Bienvenido!</p>
+    <button onclick="cerrar()">Cerrar Sesión</button>
+    `;
+}
+// función de ingreso de sesión
+
+export function ingreso() {
+  const email2 = document.getElementById('emailLogIn').value;
+  const password2 = document.getElementById('passwordLogIn').value;
+
+  firebase.auth().signInWithEmailAndPassword(email2, password2).catch((error) => {
+  // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   });
 }
-function observador() {
-  firebase.auth().onAuthStateChanged(function(user) {
+export function observador() {
+  firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       console.log('existe usuario activo');
       activeUser();
       // User is signed in.
-      let displayName = user.displayName;
-      let email = user.email;
-      let emailVerified = user.emailVerified;
-      let photoURL = user.photoURL;
-      let isAnonymous = user.isAnonymous;
-      let uid = user.uid;
-      let providerData = user.providerData;
+      const displayName = user.displayName;
+      const email = user.email;
+      const emailVerified = user.emailVerified;
+      const photoURL = user.photoURL;
+      const isAnonymous = user.isAnonymous;
+      const uid = user.uid;
+      const providerData = user.providerData;
       // ...
     } else {
       // User is signed out.
       console.log('no existe usuario activo');
       // ...
     }
-  });email-password.html
+  }); email - password.html;
 }
 observador();
-function activeUser(){
 
-    let contenido = document.getElementById('contenido');
-    contenido.innerHTML = `
-    <p> Bienvenido!</p>
-    <button onclick="cerrar()">Cerrar Sesión</button>
-    `;
-}
-//iniciando sesión con Google
-googleSignIn=()=>{
-  base_provider = new firebase.auth.GoogleAuthProvider()
-  firebase.auth().signInWithPopup(base_provider).then(function(result){
-    console.log(result)
-    console.log("Success Google Acount Linked");
-  }).catch(function(err){
-    console.log(err)
-    console.log("Failed to do")
-  })
-}
-
-//funcionalidad de Cerrar Sesión
-function cerrar() {
-    firebase.auth().signOut()
-    .then(function(){
-       console.log('Saliendo...')
+// funcionalidad de Cerrar Sesión
+export function cerrar() {
+  console.log('hola');
+  firebase.auth().signOut()
+    .then(() => {
+      console.log('Saliendo...');
     })
-    .catch(function(error){
-     console.log(error)
-    }) 
-
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 
-let db = firebase.firestore();
-
-//agregar usuario en colection
-function guardar(){
-  let nombre= document.getElementById('nombre').value;
-  let seudonimo= document.getElementById('seudonimo').value;
-  let contrasena= document.getElementById('contrasena').value;
-
-  db.collection("newUser").add({
-    firstField: nombre,
-    secondField: seudonimo,
-    tercerField: contrasena
-  })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    document.getElementById('nombre').value='';
-    document.getElementById('seudonimo').value='';
-    document.getElementById('contrasena').value='';
-
-  })
-  .catch(function(error) {
-    console.error("Error adding document: ", error);
-  });
-};
-
-
-//funcionalidad de posteo
-function post() {
-  let post = document.getElementById('posteo').value;
-  
-  db.collection("newPost").add({
+const db = firebase.firestore();
+// funcionalidad de posteo
+export function savePost() {
+  const posteo = document.getElementById('addPost').value;
+  db.collection('newPost').add({
     correo: firebase.auth().currentUser.email,
     uid: firebase.auth().currentUser.uid,
-    post: post
-    
+    post: posteo,
   })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    document.getElementById('posteo').value='';
-    
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+      document.getElementById('addPost').value = '';
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+}
 
-   })
-  .catch(function(error) {
-    console.error("Error adding document: ", error);
+export function googleSignIn() {
+  const baseProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(baseProvider).then((result) => {
+    console.log(result);
+    console.log('Success Google acount Linked');
+  }).catch((err) => {
+    console.log(err);
+    console.log('Failed to do');
   });
-};
-
-let tabla = document.getElementById('tabla');
-
-db.collection("newUSer").get().then((querySnapshot) => {
-  tabla.innerHTM = '';
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data().firstField}`);
-    tabla.innerHTML += `<p> ${doc.id}</p>
-    <p>${doc.data().firstField}</p>`
-  });
-});
+}
--- src/main.js
+++ src/main.js
@@ -1,41 +1,46 @@
 // Este es el punto de entrada de tu aplicacion
 
-//import { myFunction } from './lib/index.js';
+import {
+  ingreso, registrar, observador, cerrar, googleSignIn,
+} from './app.js';
+// myFunction();
 
-//myFunction();
-
-const root = document.getElementById("root");
-const initialHeader = document.getElementById("initialHeader");
-const btnLogIn = document.getElementById("login");
-const btnRegister = document.getElementById("register");
+const root = document.getElementById('root');
+const initialHeader = document.getElementById('initialHeader');
+const btnLogIn = document.getElementById('login');
+const btnRegister = document.getElementById('register');
 
 btnRegister.addEventListener('click', () => {
-    root.innerHTML='';
-    return root.innerHTML = 
-        `<header class="header">
+  root.innerHTML = '';
+  root.innerHTML = `<header class="header">
             <h1><img src="img/logo-boceto.png" alt=""></h1>
          </header>
         <main> 
         <form class="registerUser">
-            <button id="idGoogle">Continuar con Google</button>
-            <input class="inputText" type="text" placeholder="Nombre y apellido">
-            <input class="inputText" type="text" placeholder="Pseudonimo">
-            <input class="inputText" type="email" placeholder="Correo electronico">
-            <input class="inputText" type="password" placeholder="Contraseña">
-            <input class="inputText" type="password" placeholder="Confirma contraseña">
-            <button id="login" class="btn">Registrate</button>
+         <button id="idGoogle">Continuar con Google</button>
+         <input id=" nombreyApellido"class="inputText" type="text" placeholder="Nombre y apellido">
+         <input id="pseudonimo"class="inputText" type="text" placeholder="Pseudonimo">
+         <input id="emailRegister" class="inputText" type="email" placeholder="Correo electronico">
+         <input id="passwordRegister"class="inputText" type="password" placeholder="Contraseña">
+         <input id="passwordRegister2"class="inputText" type="password" placeholder="Confirma contraseña">
+         <button id="signIn" class="btn">Registrate</button>
+            
         </form>    
-        </main>`
-})
+        </main>`;
+  const btnsignIn = document.getElementById('signIn');
+  btnsignIn.addEventListener('click', registrar());
 
-btnLogIn.addEventListener('click',login)
-    
-    
+  const btnGoogle = document.getElementById('idGoogle');
+  btnGoogle.addEventListener('click', googleSignIn());
+});
+
+btnLogIn.addEventListener('click', login);
 function login() {
-    window.location.hash = '#home'
-    root.innerHTML='';
-    return root.innerHTML=
-    `<header class="homeLogo">
+  ingreso();
+  observador();
+  window.location.hash = '#home';
+  root.innerHTML = '';
+  root.innerHTML = `<header class="homeLogo">
     <h1><img src="img/logo-boceto.png" alt=""></h1>
      <nav class="navBar">
       <ul>
@@ -43,7 +48,8 @@ function login() {
         <li id="myWorks"><a href="#myWorks">Mis Trabajos</a></li>
         <li id="favorite"><a href="#favorite">Favoritos</a></li>
         <li id="search"><a href="#search">Buscar</a></li>
-        <li id="logout"><a href="#logout">Cerrar sesión</a></li>       
+        <li id="logout"><a href="#logout">Cerrar sesión</a></li>
+        <main>
       </ul>
      </nav>
     </header>
@@ -51,23 +57,24 @@ function login() {
         
        
     </main>
-    <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`
-  }
+    <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`;
+  const btnLogout = document.getElementById('logout');
+  btnLogout.addEventListener('click', cerrar());
+}
 
-  window.addEventListener('hashchange', () => {
-        console.log(window.location.hash)
-    if (window.location.hash === "#myWorks"){
-        root.innerHTML='';
-        return root.innerHTML=
-        `<header class="homeLogo">
-        <h1><img src="img/logo-boceto.png" alt=""></h1>
+window.addEventListener('hashchange', () => {
+  console.log(window.location.hash);
+  if (window.location.hash === '#myWorks') {
+    root.innerHTML = '';
+    root.innerHTML = `<header class ="homeLogo">
+        <h1><img src = "img/logo-boceto.png" alt=""></h1>
          <nav class="navBar">
           <ul>
             <li id="home"><a href="#home">Inicio</a></li>
             <li id="myWorks"><a href="#myWorks">Mis Trabajos</a></li>
             <li id="favorite"><a href="#favorite">Favoritos</a></li>
             <li id="search"><a href="#search">Buscar</a></li>
-            <li id="logout"><a href="#logout">Cerrar sesión</a></li>      
+            <li id="logout"><a href="#logout">Cerrar sesión</a></li>
           </ul>
          </nav>
         </header>
@@ -85,14 +92,11 @@ function login() {
         </section>
         <main>
         </main>
-        <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`
-       
-    } else if (window.location.hash === "#favorite") {
-                    
-    } else if (window.location.hash === "#home"){
-        root.innerHTML='';
-        return root.innerHTML=
-        `<header class="homeLogo">
+        <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`;
+  } else if (window.location.hash === '#favorite') {
+  } else if (window.location.hash === '#home') {
+    root.innerHTML = '';
+    root.innerHTML = `<header class="homeLogo">
         <h1><img src="img/logo-boceto.png" alt=""></h1>
         <nav class="navBar">
         <ul>
@@ -107,12 +111,10 @@ function login() {
         <main>      
        
         </main>
-        <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`
-
-    } else if (window.location.hash === "#addPost"){
-      root.innerHTML='';
-      return root.innerHTML= 
-      `<header class="homeLogo">
+        <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`;
+  } else if (window.location.hash === '#addPost') {
+    root.innerHTML = '';
+    root.innerHTML = `<header class="homeLogo">
         <h1><img src="img/logo-boceto.png" alt=""></h1>
         <nav class="navBar">
         <ul>
@@ -128,9 +130,8 @@ function login() {
       <textarea class="textPost" id="basePost" cols="30" rows="10"></textarea>
       <div class="actionButtons">
         <input type="file" class="fileAdd">
-        <button id="newPost" class="btn">Publicar</button>    
+        <button id="newPost" class="btn" onclick="savePost()">Publicar</button>    
       </div>
-      </section>`
-    }
-
-  });
+      </section>`;
+  }
+});
