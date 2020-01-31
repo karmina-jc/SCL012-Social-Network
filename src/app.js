5fa32b39addbb85b8df65a97efe1db67f6b1ae81
karmina-jc <56723755+karmina-jc@users.noreply.github.com>
2020-01-31 13:29:06 -0300

    Feat/funcionalidad (#1)
    * orden de DOM y funciones de Firebase con eventos click en main.js
    
    * se imprimen imagenes y textos de post en el main del home
    
    * detalles de estilo imagen post
    
    * inicio responsive y cambios en main para iconos
    
    Co-authored-by: Camirojasserantoni <56646322+Camirojasserantoni@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,9 +1,52 @@
-export function registrar() {
-  const email = document.getElementById('emailRegister').value;
-  const password = document.getElementById('passwordRegister').value;
 
+export const observer = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        window.location.hash = '#home'                                      
+      } else {
+        alert("Usuario Invalido")
+      }
+  }); email - password.html;    
+};
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
+// función de ingreso de sesión
+export const emailLogin = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(()=>{
+    console.log('UsuarioActivo '+ email);
+    window.location.hash = '#home'
+  })
+  .catch((error) => {
+  // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    alert('Usuario no registrado')
+  })
+};
+
+export const googleSignIn = () => {
+  const baseProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(baseProvider)
+  .then((result) => {
+    console.log(result);
+    console.log('Success Google acount Linked');
+    window.location.hash = '#home';
+
+  }).catch((err) => {
+    console.log(err);
+    console.log('Failed to do');
+    alert("Error al ingresar")
+  });
+}
+
+export function register(emailR, passR) {
+  firebase.auth().createUserWithEmailAndPassword(emailR, passR)
+  .then (()=>{
+    console.log('Usuario registrado exitosamente')
+  })
+  .catch((error) => {
   // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -13,57 +56,8 @@ export function registrar() {
   email - password.html;
 }
 
-// función de ingreso de sesión
-
-export function ingreso() {
-  const email2 = document.getElementById('emailLogIn').value;
-  const password2 = document.getElementById('passwordLogIn').value;
-
-  firebase.auth().signInWithEmailAndPassword(email2, password2)
-  .catch((error) => {
-  // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-  })
-};
-
-export function googleSignIn(){
-  const baseProvider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(baseProvider)
-  .then((result) => {
-    console.log(result);
-    console.log('Success Google acount Linked');
-    window.location.hash = '#home';
-    root.innerHTML = '';
-    root.innerHTML = `<header class="homeLogo">
-    <h1><img src="img/logo-boceto.png" alt=""></h1>
-    <nav class="navBar">
-    <ul>
-    <li id="home"><a href="#home">Inicio</a></li>
-    <li id="myWorks"><a href="#myWorks">Mis Trabajos</a></li>
-    <li id="favorite"><a href="#favorite">Favoritos</a></li>
-    <li id="search"><a href="#search">Buscar</a></li>
-    <li id="logout"><a href="#logout">Cerrar sesión</a></li>
-    <main>
-    </ul>
-    </nav>
-    </header>
-    <main id="fullMain"> 
-    
-   
-    </main>
-    <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`
-  }).catch((err) => {
-    console.log(err);
-    console.log('Failed to do');
-    alert("Error al ingresar")
-  });
-}
-
 // funcionalidad de Cerrar Sesión
-export function cerrar() {
+function cerrar() {
   console.log('hola');
   firebase.auth().signOut()
     .then(() => {
@@ -73,129 +67,116 @@ export function cerrar() {
       console.log(error);
     });
 }
-// Función para subir imagen
+
 
 
 const db = firebase.firestore();
 // funcionalidad de posteo
-export function savePost() {
-  const posteo = document.getElementById('addPost').value;
-  db.collection('newPost').add({
-    correo: firebase.auth().currentUser.email,
-    uid: firebase.auth().currentUser.uid,
-    post: posteo,
-  })
-    .then((docRef) => {
-      console.log('Document written with ID: ', docRef.id);
-      document.getElementById('addPost').value = '';
-    })
-    .catch((error) => {
-      console.error('Error adding document: ', error);
-    });
-}
+// llamando al div donde se imprimirán los post
+const divPost = document.getElementById('forPosting');
+// escuchando colección en firebase para ir imprimiendo
 
 
-let btnUploadImg = document.getElementById('uploadImage'); 
-let storageRef = firebase.storage().ref();
+
+const postimage = document.getElementById("uploadImage")
+
+
 let imagesFBRef = firebase.database().ref('ImagesFB');
-const postimage = document.getElementById('postimg')
 
-btnUploadImg.addEventListener('change', confirmUpload, false);
+//postimage.addEventListener('change',confirmUpload)
+
 // Imprimiendo imagen guardada en firebase
 function showImgFromFB(){
   imagesFBRef.on('value', function(snapshot){
     let data = snapshot.val();
     let result = '';
     for(let key in data){
-      result += `<img src="` + data[key].url + `"/>`;
+      result += `<img class="image" src="` + data[key].url + `"/>`;
     }
-    document.getElementById('forPosting').innerHTML = result;
+    document.getElementById('paraimg').innerHTML = result;
   })
 }
 // Subiendo imagen a firebase storage
-function confirmUpload () {
-  postimage.addEventListener('click', () => {
-  postimage.disabled = false;
-  const imgFile = btnUploadImg.files[0];
-  const uploadTask = storageRef.child('Images/' + imgFile.name).put(imgFile);
-  
+export const uploadImgAndText = (valueImg, valueText) => {
+  let storageRef = firebase.storage().ref();
+  const uploadTask = storageRef.child('Images/' + valueImg.name).put(valueImg);
   uploadTask.on('state_changed',
   function(snapshot) {
-}, function(error) {
- alert('error');
- // Obteniendo URL de imagen
-}, function() {
-  uploadTask.snapshot.ref.getDownloadURL()
-  .then(function(downloadURL){
-    console.log(downloadURL)
-    createNodeInFB(imgFile.name, downloadURL)
-    showImgFromFB();
+  }, function(error) {
+  alert('error');
+  
+  // Obteniendo URL de imagen
+  }, function() {
+    uploadTask.snapshot.ref.getDownloadURL()
+    .then(function(downloadURL){
+    savePost(valueText, downloadURL)       
   })
+  
 })
-})};
+};
+
+export const savePost = (textValue,imgUrl) => {
+  db.collection('Post').add({
+    correo: firebase.auth().currentUser.email,
+    uid: firebase.auth().currentUser.uid,
+    post: textValue,
+    img: imgUrl,
+  })
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+      document.getElementById('textPost').value = '';
+          
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+}
+
 // Creando nodo en firebase 
-function createNodeInFB(imgName, downloadURL){
-  imagesFBRef.push({nombre: imgName, url: downloadURL})
-}
+// // llamando al div donde se imprimirán los post
+// //const divPost = document.getElementById('forPosting');
+// // escuchando colección en firebase para ir imprimiendo
+// db.collection('postconuid').onSnapshot((querySnapshot) => {
+//   // vaciando div para que no se repitan los post
+//   divPost.innerHTML = '';
+//   querySnapshot.forEach((doc) => {
+//       console.log(`${doc.id} => ${doc.data().post}`);
+//       divPost.innerHTML +=
+//       `<div class="postDiv">
+//       <textarea class="postArea" readonly="readonly"> ${doc.data().post}</textarea>
+//       <button class="abc">Comentarios</button>
+//       </div>
+//       <div class="commentDiv" style="display:none">
+//     </div>`;
+//     // función para mostrar caja de comentarios
+//     function showComment (){
+//     document.getElementsByClassName('commentDiv').style.display = 'block';
+//     console.log('mostrar')
+//     }
+//    document.querySelectorAll('.commentDiv').forEach((element) => {
+//     element.addEventListener('click', () =>{
+//     console.log(element);
+//     divPost.innerHTML = '';
+//       })
+//     })
+//   });
+// });
 
-// Función para postear
-function posting() {
-  db.collection('postconuid').add({ // creando una coleción para agregar los datos a firebase
-    email: firebase.auth().currentUser.email,
-    uid: firebase.auth().currentUser.uid,
-    post: document.getElementById('texto').value
-  })
-    .then(function (docRef) {
-      console.log('Document written with ID: ', docRef.id); // console.log para confirmar en consola
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
-}
-// llamando al div donde se imprimirán los post
-const divPost = document.getElementById('forPosting');
-// escuchando colección en firebase para ir imprimiendo
-db.collection('postconuid').onSnapshot((querySnapshot) => {
-  // vaciando div para que no se repitan los post
-  divPost.innerHTML = '';
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().post}`);
-      divPost.innerHTML +=
-      `<div class="postDiv">
-      <textarea class="postArea" readonly="readonly"> ${doc.data().post}</textarea>
-      <button class="abc">Comentarios</button>
-      </div>
-      <div class="commentDiv" style="display:none">
-    </div>`;
-    // función para mostrar caja de comentarios
-    function showComment (){
-    document.getElementsByClassName('commentDiv').style.display = 'block';
-    console.log('mostrar')
-    }
-   document.querySelectorAll('.commentDiv').forEach((element) => {
-    element.addEventListener('click', () =>{
-    console.log(element);
-    divPost.innerHTML = '';
-      })
-    })
-  });
-});
-
-// función comentar
- function comment () {
-   console.log('comentar');
-   db.collection('postconuid').add({ // creando una coleción para agregar los datos a firebase
-    email: firebase.auth().currentUser.email,
-    uid: firebase.auth().currentUser.uid,
-    post: document.getElementById('texto').value,
-    comment: document.getElementsByClassName('commentArea').value
-  })
-    .then(function (docRef) {
-      console.log('Document written with ID: ', docRef.id); // console.log para confirmar en consola
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
- };
+// // función comentar
+//  function comment () {
+//    console.log('comentar');
+//    db.collection('postconuid').add({ // creando una coleción para agregar los datos a firebase
+//     email: firebase.auth().currentUser.email,
+//     uid: firebase.auth().currentUser.uid,
+//     post: document.getElementById('texto').value,
+//     comment: document.getElementsByClassName('commentArea').value
+//   })
+//     .then(function (docRef) {
+//       console.log('Document written with ID: ', docRef.id); // console.log para confirmar en consola
+//     })
+//     .catch(function(error) {
+//       console.error("Error adding document: ", error);
+//     });
+//  };
 
 
